a5b9c05a0f2ccc4bd9267320526e60e22cda639e
PythonDeveloper2026 <kamtarin.odam@gmail.com>
2026-05-07 07:11:49 +0500

    ALLOWED_HOSTS barcha uchun ochildi

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-test-secret-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kamtarin.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
